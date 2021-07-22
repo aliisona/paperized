f812a8a341b3c9780249317896325ab4dec18a76
AlisonaLe <>
2021-07-22 15:06:16 +0000

    Initial commit

---
+++ scholarship/scholarship.js
@@ -0,0 +1,20 @@
+function myFunction() {
+  
+  var input, filter, scholarshipList, li, a, i, txtValue;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  scholarshipList = document.getElementById("scholarshipList");
+  li = scholarshipList.getElementsByTagName("li");
+  
+  for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } 
+      else {
+        li[i].style.display = "none";
+      }
+  }
+}
+// as of 21/7 not sure why the search function still weird but working on it
